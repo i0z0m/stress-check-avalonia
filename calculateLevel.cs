35e6eb060c6fa845d2dbd23c28b1e8e530043106
i0z0m <i0z0mu@gmail.com>
2024-02-27 01:11:33 +0900

    Convert the level calculator from the TypeScript version of the app to C#

---
+++ calculateLevel.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Linq;
+
+public class LevelResult
+{
+    public bool Method1 { get; set; }
+    public bool Method2 { get; set; }
+    public List<int> Totals { get; set; }
+}
+
+public static class LevelCalculator
+{
+    public static LevelResult CalculateLevel(List<int> scores, List<List<int>> values)
+    {
+        bool method1 = scores[2] >= 77 || (scores[1] + scores[3] >= 76 && scores[2] >= 63);
+
+        List<int> totals = values.Select(value => value?.Sum() ?? 0).ToList();
+        bool method2 = totals[2] <= 12 || (totals[1] + totals[3] <= 26 && totals[2] <= 17);
+
+        return new LevelResult { Method1 = method1, Method2 = method2, Totals = totals };
+    }
+}
