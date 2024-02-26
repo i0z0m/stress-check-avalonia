0d8aeef6941147b61c40f7bee4e2c5a7063ed9f8
i0z0m <i0z0mu@gmail.com>
2024-02-26 20:15:20 +0900

    dotnet new avalonia.app -o stress-check-avalonia

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace stress_check_avalonia;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
