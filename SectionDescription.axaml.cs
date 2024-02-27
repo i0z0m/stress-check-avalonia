936b1a7c5c7aa8c17a9be8f789a576ecb3af2306
i0z0m <i0z0mu@gmail.com>
2024-02-27 21:46:07 +0900

    Display the description of the first section using the SectionDescription user control

---
+++ SectionDescription.axaml.cs
@@ -0,0 +1,60 @@
+// SectionDescription.axaml.cs
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Data;
+
+namespace stress_check_avalonia
+{
+    public partial class SectionDescription : UserControl
+    {
+        public static readonly AvaloniaProperty<string> DescriptionProperty =
+            AvaloniaProperty.Register<SectionDescription, string>(nameof(Description));
+
+        public static readonly AvaloniaProperty<bool> IsLastSectionProperty =
+            AvaloniaProperty.Register<SectionDescription, bool>(nameof(IsLastSection));
+
+        public string Description
+        {
+            get => (string)GetValue(DescriptionProperty);
+            set => SetValue(DescriptionProperty, value);
+        }
+
+        public bool IsLastSection
+        {
+            get => (bool)GetValue(IsLastSectionProperty);
+            set => SetValue(IsLastSectionProperty, value);
+        }
+
+        public SectionDescription()
+        {
+            InitializeComponent();
+            DataContext = new SectionDescriptionViewModel
+            {
+                Section = LoadSections.sections[0]
+            };
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs args)
+        {
+            base.OnPropertyChanged(args);
+
+            if (args.Property == DescriptionProperty)
+            {
+                if (DescriptionTextBlock != null)
+                {
+                    DescriptionTextBlock.Text = Description;
+                }
+            }
+
+            if (args.Property == IsLastSectionProperty)
+            {
+            }
+        }
+    }
+}
---
+++ SectionDescriptionViewModel.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel;
+
+namespace stress_check_avalonia
+{
+    public class SectionDescriptionViewModel : INotifyPropertyChanged
+    {
+        private Section _section;
+
+        public Section Section
+        {
+            get { return _section; }
+            set
+            {
+                if (_section != value)
+                {
+                    _section = value;
+                    OnPropertyChanged(nameof(Section));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
