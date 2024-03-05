799042c14e1b015bf4d2db8078fcacf179f98a40
i0z0m <i0z0mu@gmail.com>
2024-03-05 22:29:37 +0900

    Add AvaloniaProperty for QuestionIndex and use it

--- Views/QuestionText.axaml.cs
+++ Views/QuestionText.axaml.cs
@@ -1,3 +1,4 @@
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
@@ -5,6 +6,10 @@ namespace stress_check_avalonia
 {
     public partial class QuestionText : UserControl
     {
+        // Add AvaloniaProperty for QuestionIndex
+        public static readonly AvaloniaProperty QuestionIndexProperty =
+            AvaloniaProperty.Register<QuestionText, int>("QuestionIndex");
+
         public QuestionText()
         {
             InitializeComponent();
@@ -16,7 +21,13 @@ namespace stress_check_avalonia
             AvaloniaXamlLoader.Load(this);
         }
 
-        public int QuestionIndex { get; set; }
+        // Use AvaloniaProperty for QuestionIndex
+        public int QuestionIndex
+        {
+            get { return (int)(GetValue(QuestionIndexProperty) ?? 0); } // Cast to int and handle possible null value
+            set { SetValue(QuestionIndexProperty, value); }
+        }
+
         public Question DisplayedQuestion { get; private set; }
 
         protected override void OnDataContextChanged(System.EventArgs e)
@@ -28,8 +39,8 @@ namespace stress_check_avalonia
                 DisplayedQuestion = viewModel.Questions[QuestionIndex];
                 viewModel.DisplayedQuestion = DisplayedQuestion;
 
-            // Convert the Id to string and display it
-            var questionIdTextBlock = this.FindControl<TextBlock>("QuestionIdTextBlock");
+                // Convert the Id to string and display it
+                var questionIdTextBlock = this.FindControl<TextBlock>("QuestionIdTextBlock");
                 if (questionIdTextBlock != null)
                 {
                     questionIdTextBlock.Text = DisplayedQuestion.Id.ToString();
