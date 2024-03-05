63f05c0f98390ce0e1cbe5296fc0961f72ccfa51
i0z0m <i0z0mu@gmail.com>
2024-03-06 01:48:34 +0900

    Dynamically generate QuestionText and ChoiceButtons user controls

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,6 +1,5 @@
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
+using Avalonia;
 
 namespace stress_check_avalonia
 {
@@ -10,11 +9,40 @@ namespace stress_check_avalonia
         {
             InitializeComponent();
             this.Title = "ストレスチェック実施プログラム";
-        }
 
-        private void InitializeComponent()
-        {
-            AvaloniaXamlLoader.Load(this);
+            // Use the Questions from SectionViewModel
+            var questions = SectionViewModel.Instance.Questions;
+
+            for (int i = 0; i < questions.Count; i++)
+            {
+                var questionText = new QuestionText
+                {
+                    QuestionIndex = i
+                };
+
+                var choiceButtons = new ChoiceButtons
+                {
+                    HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Right,
+                    Margin = new Thickness(0, 0, 80, 0)
+                };
+
+                var questionGrid = new Grid
+                {
+                    ColumnDefinitions =
+                    {
+                        new ColumnDefinition(1, GridUnitType.Star),
+                        new ColumnDefinition(0, GridUnitType.Auto)
+                    }
+                };
+
+                Grid.SetColumn(questionText, 0);
+                Grid.SetColumn(choiceButtons, 1);
+
+                questionGrid.Children.Add(questionText);
+                questionGrid.Children.Add(choiceButtons);
+
+                QuestionsPanel.Children.Add(questionGrid);
+            }
         }
     }
 }
