606d17b483578a74d31efafb22ba13142eb86483
i0z0m <i0z0mu@gmail.com>
2024-03-07 17:50:18 +0900

    Create new user controls NextButton and BackButton that are fixed at the bottom of the screen.

---
+++ Views/BackButtons.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace stress_check_avalonia
+{
+    public partial class BackButtons : UserControl
+    {
+        public BackButtons()
+        {
+            InitializeComponent();
+        }
+
+        public void ClickHandler(object sender, RoutedEventArgs args)
+        {
+        }
+    }
+}
---
+++ Views/NextButton.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace stress_check_avalonia
+{
+    public partial class NextButton : UserControl
+    {
+        public NextButton()
+        {
+            InitializeComponent();
+        }
+
+        public void ClickHandler(object sender, RoutedEventArgs args)
+        {
+        }
+    }
+}
