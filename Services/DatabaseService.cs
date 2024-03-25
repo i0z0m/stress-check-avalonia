619741d2468bf2de59cd3db79f88acc50ceac061
i0z0m <i0z0mu@gmail.com>
2024-03-25 14:45:12 +0900

    Install Microsoft.Data.Sqlite and create stress_check_db.sqlite

---
+++ Services/DatabaseService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Data.Sqlite;
+
+public class DatabaseService
+{
+    private const string ConnectionString = "Data Source=stress_check_db.sqlite";
+
+    public void CreateDatabase()
+    {
+        using (var connection = new SqliteConnection(ConnectionString))
+        {
+            connection.Open();
+
+            var command = connection.CreateCommand();
+            command.CommandText =
+            @"
+                CREATE TABLE IF NOT EXISTS questions (
+                    id INTEGER PRIMARY KEY,
+                    score INTEGER NOT NULL
+                )
+            ";
+
+            command.ExecuteNonQuery();
+        }
+    }
+
+    // TODO InsertOrUpdateQuestionScoreメソッドとGetQuestionScoreメソッドは後で追加
+}
