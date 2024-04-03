bbccccb1594bd77f0ef57c04fc1679b86725aa92
i0z0m <i0z0mu@gmail.com>
2024-04-03 12:07:32 +0900

    Prepare dotnet development environment with nix flake instead of docker container

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A Nix-flake-based C# development environment";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            dotnet-sdk_8
+            mono
+            msbuild
+            nuget
+          ];
+        };
+      }
+    );
+}
