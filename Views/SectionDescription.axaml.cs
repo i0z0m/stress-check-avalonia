882d40ee3bd3b1f803d2d5b9b2a58103ddd792e3
i0z0m <i0z0mu@gmail.com>
2024-04-13 16:22:14 +0900

    Simplified SectionDescription by leveraging ReactiveUI in SectionViewModel

--- Views/SectionDescription.axaml.cs
+++ Views/SectionDescription.axaml.cs
@@ -1,30 +1,11 @@
-using StressCheckAvalonia.ViewModels;
-using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using StressCheckAvalonia.ViewModels;
 
 namespace StressCheckAvalonia.Views
 {
     public partial class SectionDescription : UserControl
     {
-        public static readonly AvaloniaProperty<string> DescriptionProperty =
-            AvaloniaProperty.Register<SectionDescription, string>(nameof(Description));
-
-        public static readonly AvaloniaProperty<bool> IsLastSectionProperty =
-            AvaloniaProperty.Register<SectionDescription, bool>(nameof(IsLastSection));
-
-        public string Description
-        {
-            get => (string)GetValue(DescriptionProperty);
-            set => SetValue(DescriptionProperty, value);
-        }
-
-        public bool IsLastSection
-        {
-            get => (bool)GetValue(IsLastSectionProperty);
-            set => SetValue(IsLastSectionProperty, value);
-        }
-
         public SectionDescription()
         {
             InitializeComponent();
@@ -34,20 +15,6 @@ namespace StressCheckAvalonia.Views
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
-        }
-
-        protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs args)
-        {
-            base.OnPropertyChanged(args);
-
-            if (args.Property == DescriptionProperty && DescriptionTextBlock != null)
-            {
-                DescriptionTextBlock.Text = Description;
-            }
-
-            if (args.Property == IsLastSectionProperty)
-            {
-            }
         }
     }
 }
