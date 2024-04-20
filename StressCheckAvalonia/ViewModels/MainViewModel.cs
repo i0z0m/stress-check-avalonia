28c045cece248cc9c62a9fcc627587c49c66c8eb
i0z0m <i0z0mu@gmail.com>
2024-04-21 07:09:22 +0900

    Fix the data context to display the screen correctly on the desktop version

--- StressCheckAvalonia/ViewModels/MainViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using ReactiveUI;
-namespace StressCheckAvalonia.ViewModels;
-
-public class MainViewModel : ReactiveObject
-{
-#pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
-}
